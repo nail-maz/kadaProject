2ba98d499cd4f4583ad4de1919b62908c75e3ac5
akader36 <akader36@gmail.com>
2020-07-09 15:39:37 +0100

    تصميم واجهة المستخدم

--- src/Controller/Home.java
+++ src/Controller/Home.java
@@ -1,5 +1,21 @@
 package Controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Home {
+    public void zone() throws IOException {
+        Stage primaryStage=new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("../Views/zone.fxml"));
+        Scene scene=new Scene(root,1152,610);
+        primaryStage.setTitle("لوحة التحكم المستخدمين  ");
+        primaryStage.setScene( scene );
+        primaryStage.show();
+
+    }
 
 }
